36089908eb11f6be22dd84921015ffa4e2fa5513
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2021-10-22 16:46:33 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -60,7 +60,7 @@ void ledAlerter(float Threshold,struct StatscomputedStats)
 }
 
 
-void check_and_alert ( float maxThreshold,alerter_funcptr alerter[], struct Stats computedStats)
+void check_and_alert ( float maxThreshold,alerter_funcptr alerters[], struct Stats computedStats)
 {
     if(Stats.max > maxThreshold)
     {
