0878aca5f34a08b1fe2d92436bc6521ca85e1ff9
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2021-10-22 18:16:10 +0530

    Create alerts.c

---
+++ alerts.c
@@ -0,0 +1,14 @@
+void emailAlerter(float Threshold,struct Stats computedStats)
+{
+   if(computedStats.max > Threshold)
+   {
+       emailAlertCallCount=1;
+   }
+}
+void ledAlerter(float Threshold,struct Stats computedStats)
+{
+   if(computedStats.max > Threshold)
+   {
+       ledAlertCallCount=1;
+   }
+}
