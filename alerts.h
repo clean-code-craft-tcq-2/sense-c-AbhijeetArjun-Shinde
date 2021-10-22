69a14ef9409880eec6fefbfe4baf8b31971d0c42
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2021-10-22 18:19:06 +0530

    Create alerts.h

---
+++ alerts.h
@@ -0,0 +1,6 @@
+#include "stats.h"
+#include "alerts.h"
+#include "stdio.h"
+#include <math.h>
+void emailAlerter(float Threshold,struct Stats computedStats);
+void ledAlerter(float Threshold,struct Stats computedStats);
