70e762f294671eb6e24ec3d2aa13f2dbf113a6f6
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2021-10-22 09:31:26 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,43 @@
 #include "stats.h"
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    float total_sum = 0 ;
+    float max_value = numberset[0];
+    float min_value = numberset[0];
+    
+    if(setlength != 0 )
+    {
+        for(i =0 ; i < setlength ; i++)
+        {
+            total_sum += numberset[i];
+        
+            if( max_value > numberset[i] )
+            {
+                max_value = numberset[i] 
+            }
+        
+            if(min_value > numberset[i])
+            {
+                min_value=numberset[i]
+            }
+        }
+   
+        s.average = total_sum/setlength;
+        s.min = min_value;
+        s.max = max_value;
+    }
+    else
+    {
+        s.average = NaN;
+        s.min = NaN;
+        s.max = NaN;  
+    }
+    
+    return s;
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+
